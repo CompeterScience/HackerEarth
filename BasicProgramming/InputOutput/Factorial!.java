96489b4177b2ec1df1312912681507420fe09552
CompeterScience <39387779+CompeterScience@users.noreply.github.com>
2018-05-22 00:12:11 -0700

    Create Factorial!.java

---
+++ BasicProgramming/InputOutput/Factorial!.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class TestClass {
+    public static void main(String args[] ) throws Exception {
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+        int numFac = 1;
+        for(int i=0; i<num; i++) {
+            numFac = numFac * (i+1);
+        }a
+        System.out.println(numFac);
+    }
+}
